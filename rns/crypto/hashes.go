f90da0d7f17c4a73a09e557145c1412bd068f3af
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2026-03-12 11:18:09 -0400

    Add hashes.go
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

---
+++ rns/crypto/hashes.go
@@ -0,0 +1,25 @@
+// Copyright 2026 Glenn Lewis. All rights reserved.
+//
+// Use of this source code is governed by the Reticulum License
+// that can be found in the LICENSE file.
+
+package crypto
+
+import (
+	"crypto/sha256"
+	"crypto/sha512"
+)
+
+// SHA256 returns the SHA-256 digest of the data.
+func SHA256(data []byte) []byte {
+	digest := sha256.New()
+	digest.Write(data)
+	return digest.Sum(nil)
+}
+
+// SHA512 returns the SHA-512 digest of the data.
+func SHA512(data []byte) []byte {
+	digest := sha512.New()
+	digest.Write(data)
+	return digest.Sum(nil)
+}
---
+++ rns/crypto/hashes_test.go
@@ -0,0 +1,39 @@
+// Copyright 2026 Glenn Lewis. All rights reserved.
+//
+// Use of this source code is governed by the Reticulum License
+// that can be found in the LICENSE file.
+
+package crypto
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestSHA256(t *testing.T) {
+	data := []byte("hello world")
+	got := SHA256(data)
+	want := []byte{185, 77, 39, 185, 147, 77, 62, 8,
+		165, 46, 82, 215, 218, 125, 171, 250,
+		196, 132, 239, 227, 122, 83, 128, 238,
+		144, 136, 247, 172, 226, 239, 205, 233}
+	if !bytes.Equal(got, want) {
+		t.Errorf("SHA256 = %v, want %v", got, want)
+	}
+}
+
+func TestSHA512(t *testing.T) {
+	data := []byte("hello world")
+	got := SHA512(data)
+	want := []byte{48, 158, 204, 72, 156, 18, 214, 235,
+		76, 196, 15, 80, 201, 2, 242, 180,
+		208, 237, 119, 238, 81, 26, 124, 122,
+		155, 205, 60, 168, 109, 76, 216, 111,
+		152, 157, 211, 91, 197, 255, 73, 150,
+		112, 218, 52, 37, 91, 69, 176, 207,
+		216, 48, 232, 31, 96, 93, 207, 125,
+		197, 84, 46, 147, 174, 156, 215, 111}
+	if !bytes.Equal(got, want) {
+		t.Errorf("SHA512 = %v, want %v", got, want)
+	}
+}
