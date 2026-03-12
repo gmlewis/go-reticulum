aaf2340b5d9ce31197334210ae9278f92d009414
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2026-03-12 10:58:11 -0400

    Add version.go
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

---
+++ rns/version.go
@@ -0,0 +1,9 @@
+// Copyright 2026 Glenn Lewis. All rights reserved.
+//
+// Use of this source code is governed by the Reticulum License
+// that can be found in the LICENSE file.
+
+package rns
+
+// VERSION is on a line by itself to enable auto version bumping:
+const VERSION = "0.1.0"
